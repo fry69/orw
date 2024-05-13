3113cb11a27c65d8c840785174953adfe7a5ad63
fry69 <142489379+fry69@users.noreply.github.com>
2024-05-13 13:06:50 +0200

    feat: add systemd service

---
+++ tools/install_service.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+systemctl="systemctl --user --quiet"
+service_dir="$HOME/.config/systemd/user/"
+
+mkdir -p "$service_dir"
+
+cp "./tools/orw.service" "$service_dir"
+
+(
+    $systemctl daemon-reload
+    $systemctl enable orw
+    $systemctl is-active orw.service
+)
+
+# shellcheck disable=SC2181
+if [ $? -eq 0 ]
+then
+    echo "OpenRouter API Watcher service sucessfully installed"
+else
+    echo "Service installation failed, check above for error messages."
+fi
