ad9bdfd3b4b9d30c151404460994d49848d5b4bd
fry69 <142489379+fry69@users.noreply.github.com>
2025-08-26 16:46:24 +0200

    chore: add vite inspect plugin

--- orw-deno/vite.config.ts
+++ orw-deno/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import { fresh } from "@fresh/plugin-vite";
 import tailwind from "@tailwindcss/vite";
+import inspect from "vite-plugin-inspect";
 
 export default defineConfig({
   plugins: [
     fresh(),
     tailwind(),
+    inspect(),
   ],
 });
