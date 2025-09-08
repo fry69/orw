014fc6752dfe04466a2735917012f84c2da4c8e8
fry69 <142489379+fry69@users.noreply.github.com>
2025-09-08 18:52:45 +0200

    chore: remove vite-inspect for now

--- orw-deno/vite.config.ts
+++ orw-deno/vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from "vite";
 import { fresh } from "@fresh/plugin-vite";
 import tailwind from "@tailwindcss/vite";
-import inspect from "vite-plugin-inspect";
 
 export default defineConfig({
   plugins: [
     fresh(),
     tailwind(),
-    inspect({
-      build: true,
-      outputDir: "static/vite-inspect",
-    }),
   ],
 });
