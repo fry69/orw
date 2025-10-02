f514f58b383f6b484060bb0d89148b25818b6c8e
fry69 <142489379+fry69@users.noreply.github.com>
2025-10-02 17:13:02 +0200

    feat: add vite-plugin-localcaddy

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import { fresh } from "@fresh/plugin-vite";
 import tailwind from "@tailwindcss/vite";
+import domain from "@fry69/vite-plugin-localcaddy";
 
 export default defineConfig({
   plugins: [
     fresh(),
     tailwind(),
+    domain(),
   ],
 });
